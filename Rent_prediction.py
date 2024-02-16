7dd87d00b3b74936e5b4eac7881e8c66d053e7c3
Ganesh7134 <136222889+Ganesh7134@users.noreply.github.com>
2024-02-16 08:11:26 -0600

    Add files via upload

--- Rent_prediction.py
+++ Rent_prediction.py
@@ -2,110 +2,149 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+from streamlit_lottie import st_lottie
+import json
 
 df = pd.read_csv("House_rent_data.csv")
-\
+
+container = st.container()
+try:
+    with container:
+        @st.cache_data(ttl=60 * 60)
+        def load_lottie_file(filepath : str):
+            with open(filepath, "r") as f:
+                gif = json.load(f)
+            st_lottie(gif, speed=1, width=650, height=450)
+            
+        load_lottie_file("house.json")
+except:
+    print("Don't raise exception")
+
 st.title("Smart Predictive Modeling for Rental  Property Prices.")
 st.warning("some columns selections having 0 and 1 means **no** and **yes** respectively.")
-col1 , col2 = st.columns(2)
 
 
-with col1:
-    type = st.selectbox("type of the house", df["type"].unique(),key=2)
-    lift = st.selectbox("select you want lift or not: ", df["lift"].unique(),key = 3)
-    swimming = st.selectbox("select you want swimming pool or not: ", df["swimming_pool"].unique(),key = 4)
-    parking = st.selectbox("select which parking facility do you want : ", df["parking"].unique(),key = 5)
-    building = st.selectbox("select which type of building you want : ", df["building_type"].unique(),key = 6)
-    security = st.selectbox("select security facility or not: ", df["security"].unique(),key = 7)
-    lease_type = st.selectbox("select lease type: ", df["lease_type"].unique(),key = 8)
-    locality = st.selectbox("select locality: ", df["locality"].unique(),key = 9)
-    furnishing = st.selectbox("select furnishing type: ", df["furnishing"].unique(),key = 10)
-    water_type = st.selectbox("select water facility: ", df["water_supply"].unique(),key = 11)
-    facing = st.selectbox("select facing: ", df["facing"].unique(),key = 12)
-    negotiable = st.selectbox("select negotiable status: ", df["negotiable"].unique(),key = 13)
-    latitude = st.slider("select latitude: ", min_value = df["latitude"].min() , max_value = df["latitude"].max() , key=19)
-with col2:
-    property_size = st.text_input("Enter the property size: ", key = 1)
-    st.warning(f"**min value** : {df['property_size'].min()} and **max_value**: {df['property_size'].max()}")
-    property_age = st.text_input("Enter property age :",key=14)
-    st.warning(f"**min value** : {df['property_age'].min()} and **max_value**: {df['property_age'].max()}")
-    bathrooms = st.text_input("Enter bathrooms count :",key=15)
-    st.warning(f"**min value** : {df['bathroom'].min()} and **max_value**: {df['bathroom'].max()}")
-    cup_board = st.text_input("Enter cup_board count :",key=16)
-    st.warning(f"**min value** : {df['cup_board'].min()} and **max_value**: {df['cup_board'].max()}")
-    floor = st.text_input("Enter floor count :",key=17)
-    st.warning(f"**min value** : {df['total_floor'].min()} and **max_value**: {df['total_floor'].max()}")
-    balconies = st.text_input("Enter how many balconies do you want : ",key=18)
-    st.warning(f"**min value** : {df['balconies'].min()} and **max_value**: {df['balconies'].max()}")
-    longitude = st.slider("select longitude: ", min_value = df["longitude"].min() , max_value = df["longitude"].max() , key=20)
+
+with st.sidebar:
+    locality = st.selectbox("select loction:",df["locality"].unique(),key=30)
+    activation_date = st.selectbox("select activation_date:",df["activation_date"].unique(),key=31)
+    type = st.selectbox("select property type : ",df["type"].unique(),key=1)
+    lease_type = st.selectbox("select lease type : ",df["lease_type"].unique(),key=2)
+    gym = st.selectbox("select gym status :",df["gym"].unique(),key=3)
+    lift = st.selectbox("select lift status :",df["lift"].unique(),key=4)
+    swimming = st.selectbox("select swimming_pool status :",df["swimming_pool"].unique(),key=5)
+    negotiate = st.selectbox("select negotiate status :",df["negotiable"].unique(),key=6)
+    furnish = st.selectbox("select furnishing category:",df["furnishing"].unique(),key=7)
+    parking = st.selectbox("select parking category:",df["parking"].unique(),key=8)
+    facing = st.selectbox("select direction of Rental house:",df["facing"].unique(),key=9)
+    water = st.selectbox("select water source:",df["water_supply"].unique(),key=10)
+    building = st.selectbox("select building type:",df["building_type"].unique(),key=11)
+    security = st.selectbox("select security status:",df["security"].unique(),key=12)
+    park = st.selectbox("select park status:",df["park"].unique(),key=13)
+    Ac = st.selectbox("select Ac status:",df["Ac"].unique(),key=14)
+    property_size = st.text_input("Enter property_size:",key=15)
+    st.warning(f"**min_value** = {df['property_size'].min()} and **max_value**  ={df['property_size'].max()}")
+    property_age = st.text_input("Enter property_age:",key=16)
+    st.warning(f"**min_value** = {df['property_age'].min()} and **max_value**  ={df['property_age'].max()}")
+    bathroom = st.text_input("Enter bathroom_count:",key=17)
+    st.warning(f"**min_value** = {df['bathroom'].min()} and **max_value**  ={df['bathroom'].max()}")
+    cup_board = st.text_input("Enter cup_board_count:",key=18)
+    st.warning(f"**min_value** = {df['cup_board'].min()} and **max_value**  ={df['cup_board'].max()}")
+    lower_floor = st.text_input("Enter lower_floor_count:",key=19)
+    st.warning(f"**min_value** = {df['floor'].min()} and **max_value**  ={df['floor'].max()}")
+    higher_floor = st.text_input("Enter higher_floor_count:",key=20)
+    st.warning(f"**min_value** = {df['total_floor'].min()} and **max_value**  ={df['total_floor'].max()}")
+    balcnies = st.text_input("Enter balcony_count:",key=21)
+    st.warning(f"**min_value** = {df['balconies'].min()} and **max_value**  ={df['balconies'].max()}")
+    latitude = st.selectbox("select any latitude values in selected location:",df.loc[df["locality"] == locality]["latitude"].unique())
+    longitude = st.selectbox("select any longitude values in selected location:",df.loc[df["locality"] == locality]["longitude"].unique())
 
 def result():
-    # Load Random Forest model
     with open('randomforest_model.pkl', 'rb') as file:
         rfr = pickle.load(file)
-
-    # Load scaler
-    with open('Standard_scaler.pkl', 'rb') as f:
+    with open('rent_scale.pkl', 'rb') as f:
         ss = pickle.load(f)
-
-    # Load OneHotEncoders
-    with open('type.pkl', 'rb') as f:
+    with open("type_ohe.pkl", "rb") as f:
         Ohe1 = pickle.load(f)
-
-    with open('lift.pkl', 'rb') as f:
+    with open("lease_ohe.pkl", "rb") as f:
         Ohe2 = pickle.load(f)
-
-    with open('swimming.pkl', 'rb') as f:
+    with open("gym_ohe.pkl", "rb") as f:
         Ohe3 = pickle.load(f)
-
-    with open('parking.pkl', 'rb') as f:
+    with open("lift_ohe.pkl", "rb") as f:
         Ohe4 = pickle.load(f)
-
-    with open('building.pkl', 'rb') as f:
+    with open("swimming_ohe.pkl", "rb") as f:
         Ohe5 = pickle.load(f)
-
-    with open('security.pkl', 'rb') as f:
+    with open("negotiate_ohe.pkl", "rb") as f:
         Ohe6 = pickle.load(f)
-
-    with open('lease_type.pkl', 'rb') as f:
+    with open("furnish_ohe.pkl", "rb") as f:
         Ohe7 = pickle.load(f)
-
-    with open('locality.pkl', 'rb') as f:
+    with open("parking_ohe.pkl", "rb") as f:
         Ohe8 = pickle.load(f)
-
-    with open('furnishing.pkl', 'rb') as f:
+    with open("facing_ohe.pkl", "rb") as f:
         Ohe9 = pickle.load(f)
-
-    with open('water.pkl', 'rb') as f:
+    with open("water_ohe.pkl", "rb") as f:
         Ohe10 = pickle.load(f)
-
-    with open('facing.pkl', 'rb') as f:
+    with open("building_ohe.pkl", "rb") as f:
         Ohe11 = pickle.load(f)
-
-    with open('negotiable.pkl', 'rb') as f:
+    with open("security_ohe.pkl", "rb") as f:
         Ohe12 = pickle.load(f)
-    new_test = np.array([[np.log(float(property_size)),np.log(float(property_age)),np.log(float(bathrooms)),np.log(float(cup_board)),np.log(float(floor)),np.log(float(balconies)),latitude,longitude,type,lift,swimming,parking,building,security,lease_type,locality,furnishing,water_type,facing,negotiable]])
+    with open("park_ohe.pkl", "rb") as f:
+        Ohe13 = pickle.load(f)
+    with open("Ac_ohe.pkl", "rb") as f:
+        Ohe14 = pickle.load(f)
+    with open("locality_le.pkl", "rb") as f:
+        le1 = pickle.load(f)
+    with open("ac_date_le.pkl", "rb") as f:
+        le2 = pickle.load(f)
 
-    new_test_type = Ohe1.transform(new_test[:, [8]]).toarray()
-    new_test_lift = Ohe2.transform(new_test[:, [9]]).toarray()
-    new_test_swimming = Ohe3.transform(new_test[:,[10]]).toarray()
-    new_test_parking = Ohe4.transform(new_test[:,[11]]).toarray()
-    new_test_building = Ohe5.transform(new_test[:,[12]]).toarray()
-    new_test_security = Ohe6.transform(new_test[:,[13]]).toarray()
-    new_test_lease = Ohe7.transform(new_test[:,[14]]).toarray()
-    new_test_local = Ohe8.transform(new_test[:,[15]]).toarray()
-    new_test_furnish = Ohe9.transform(new_test[:,[16]]).toarray()
-    new_test_water = Ohe10.transform(new_test[:,[17]]).toarray()
-    new_test_facing = Ohe11.transform(new_test[:,[18]]).toarray()
-    new_test_negotiate = Ohe12.transform(new_test[:,[19]]).toarray()
+    new_test = np.array([[np.log(float(property_size)),np.log(float(property_age)),np.log(float(bathroom)),np.log(float(cup_board)),np.log(float(lower_floor)),np.log(float(higher_floor)),np.log(float(balcnies)),latitude,longitude,type,locality,activation_date,lease_type,gym,lift,swimming,negotiate,furnish,parking,facing,water,building,security,park,Ac]])
+
+    new_test_type = Ohe1.transform(new_test[:, [9]]).toarray()
+
+    new_test_locality = le1.transform(new_test[:,[10]])
+
+    # Reshape the new_test_locality array
+    new_test_locality = new_test_locality.reshape(-1,1)
 
 
+    new_test_ac_date = le2.transform(new_test[:,[11]])
 
-    new_test = np.concatenate((new_test[:, [0,1,2,3,4,5,6,7]], new_test_type , new_test_lift , new_test_swimming , new_test_parking , new_test_building , new_test_security , new_test_lease , new_test_local , new_test_furnish , new_test_water , new_test_facing , new_test_negotiate), axis=1)
+    new_test_ac_date = new_test_ac_date.reshape(-1,1)
+
+    new_test_lease = Ohe2.transform(new_test[:, [12]]).toarray()
+
+    new_test_gym = Ohe3.transform(new_test[:, [13]]).toarray()
+
+    new_test_lift = Ohe4.transform(new_test[:, [14]]).toarray()
+
+    new_test_swimming = Ohe5.transform(new_test[:, [15]]).toarray()
+
+    new_test_negotiate = Ohe6.transform(new_test[:, [16]]).toarray()
+
+    new_test_furnish = Ohe7.transform(new_test[:, [17]]).toarray()
+
+    new_test_parking = Ohe8.transform(new_test[:, [18]]).toarray()
+
+    new_test_facing = Ohe9.transform(new_test[:, [19]]).toarray()
+
+    new_test_water = Ohe10.transform(new_test[:, [20]]).toarray()
+
+    new_test_building = Ohe11.transform(new_test[:, [21]]).toarray()
+
+    new_test_security = Ohe12.transform(new_test[:, [22]]).toarray()
+
+    new_test_park = Ohe13.transform(new_test[:, [23]]).toarray()
+
+    new_test_Ac = Ohe14.transform(new_test[:, [24]]).toarray()
+
+    new_test = np.concatenate((new_test[:, [0,1,2,3,4,5,6,7,8]], new_test_type , new_test_locality , new_test_ac_date , new_test_lease , new_test_gym , new_test_lift , new_test_swimming , new_test_negotiate , new_test_furnish , new_test_parking , new_test_facing , new_test_water,new_test_building,new_test_security,new_test_park,new_test_Ac), axis=1)
+
     # print(new_test[0].shape)
+
+
     new_test1 = ss.transform(new_test)
     new_pred = rfr.predict(new_test1)
-    st.write(f'## :Orange[Rental property price: {np.exp(new_pred)}]')
+    st.title(f'Rental property price Rs: :blue[{int(np.exp(new_pred))}/-]')
 
 button = st.button("preducting rental property price",type="primary",use_container_width=True)
 
